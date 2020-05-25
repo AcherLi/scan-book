fd66c6655a186cf30bc55d6505ad8ec9705be262
lizonghao <794093538@qq.com>
2020-05-25 22:12:30 +0800

    feat: init

--- functions/getBookInfo/index.js
+++ functions/getBookInfo/index.js
@@ -6,12 +6,16 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => new Promise((resolve, reject) => {
-  request.post({
-    url: 'http://weixin.hfbus.cn/HFRTB/siteList',
-    form: { sitename: event.sitename, flag: event.flag }
+  request({
+    method: 'GET',
+    url: 'https://book.feelyou.top/isbn/' + event.isbn,
+    headers: {
+      "Sec-Fetch-Dest": "document",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+    }
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      resolve(JSON.parse(body).data);
+      resolve(body);
     }
   })
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,119 +1,35 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
-// 引用百度地图微信小程序JSAPI模块 
-var bmap = require('../../utils/bmap-wx.min.js'); 
-
-// 引入工具类
-let utils = require('../../utils/util.js');
-
+// pages/index/index.js
 Page({
   data: {
-    markers: [],
-    latitude: '',
-    longitude: '',
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    tableData: []
+    src: '',
+    result: []
   },
-  // 跳转到搜索列表
-  bindKeyInput: function (e) {
-    wx.navigateTo({
-      url: `/pages/busList/busList`
-    })
-  },
-  // 跳转到站点全部路线列表
-  navigateToAll(e) {
-    let {name, lat, lng, dis, address } = { ...e.currentTarget.dataset}
-    wx.navigateTo({
-      url: `/pages/busAll/busAll?address=${address}&name=${name}&lng=${lng}&lat=${lat}&dis=${dis}`
-    })
-  },
-  //事件处理函数
-  switch: function(e) {
-    let { k, kk } = { ...e.currentTarget.dataset };
-    let tableData = [...this.data.tableData];
-    if (tableData[k].line[kk].detail.LineName) {
-      tableData[k].line[kk].status = tableData[k].line[kk].status == 1 ? 0 : 1;
-      this.setData({ tableData: tableData});
-    }
-  },
-  onLoad: function () {
-    // 查询是否获得了定位授权
-    wx.getSetting({
-      success(res) {
-        let authroize = res.authSetting["scope.userLocation"]
-        if (!authroize) {
-          wx.getSetting({
-            success(res) {
-              if (!res.authSetting['scope.userLocation']) {
-                wx.authorize({
-                  scope: 'scope.userLocation',
-                  success() {
-                    // 用户已经同意小程序使用定位功能，后续调用 wx.startRecord 接口不会弹窗询问
-                  }
-                })
-              }
-            }
-          })
-        }
-      }
-    })
-    wx.showLoading({title: '数据加载中'})
-    const BMap = new bmap.BMapWX({
-      ak: 'ohE2mpSMfPfRTk9yCGW8lC95mUuB4GS0'
-    });
-    // 发起regeocoding检索请求 
-    BMap.regeocoding({
-      fail: res => {
-        console.log(res);
-      },
-      success: res => {
-        let wxMarkerData = res.wxMarkerData;
-        this.setData({
-          markers: wxMarkerData,
-          latitude: wxMarkerData[0].latitude,
-          longitude: wxMarkerData[0].longitude
-        })
-        this.nearLineQuery()
-      }
-    });
-  },
-  nearLineQuery () {
+  handlePhoto() {
     let that = this
-    wx.cloud.callFunction({
-      name: 'nearLineQuery',
-      data: {
-        lat: this.data.latitude,
-        lng: this.data.longitude,
-      },
-      success: res => {
-        that.setData({ tableData: res.result.list })
-      },
-      fail: res => {
-        wx.showModal({
-          title: '提示',
-          content: '获取数据失败，请稍后重试',
-          success: res => { }
+    // 调用扫描api
+    wx.scanCode({
+      success(code) {
+        console.log(code)
+        let isbn = code.result
+        // 调用云函数
+        wx.cloud.callFunction({
+          name: 'getBookInfo',
+          data: { isbn },
+          success: res => {
+            that.setData({ result: res.result })
+          },
+          fail: res => {
+            wx.showModal({
+              title: '提示',
+              content: '获取数据失败，请稍后重试',
+              success: res => { }
+            })
+          },
+          complete: res => {
+            wx.hideLoading();
+          }
         })
-      },
-      complete: res => {
-        wx.hideLoading();
-        wx.stopPullDownRefresh();
       }
     })
-  },
-  onShareAppMessage: function (res) {
-    if (res.from === 'button') {
-      // 来自页面内转发按钮
-      console.log(res.target)
-    }
-    return {
-      title: '这是一个实用的小程序',
-      path: '/pages/index/index'
-    }
-  },
-  onUnload() {
-    wx.hideLoading();
   }
 })
